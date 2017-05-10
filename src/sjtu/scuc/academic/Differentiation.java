a470ca722b32d1aafbaa9117f5f51d3b56a0f6b3
zsp1197 <zsp1197@163.com>
2017-05-10 14:01:20 +0800

    成功获取全部边际价格，另外也完成了针对单目标的边际价格求取

--- src/sjtu/scuc/academic/Differentiation.java
+++ src/sjtu/scuc/academic/Differentiation.java
@@ -13,81 +13,86 @@ import java.util.List;
  */
 public class Differentiation {
     SCUCData scucData;
-    double penalty=99999999;
+    double penalty = 99999999;
+
     public Differentiation(SCUCData scucData) {
         this.scucData = scucData;
     }
-    public double getMpriceT(Calresult calresult,int t){
-        List<Double> mprices=new ArrayList<Double>();
-        int no_of_ti=scucData.getTiNum();
-        int no_of_gen=scucData.getGenNum();
+
+    public double getMpriceT(Calresult calresult, int t) {
+        List<Double> mprices = new ArrayList<Double>();
+        int no_of_ti = scucData.getTiNum();
+        int no_of_gen = scucData.getGenNum();
         for (int i = 0; i < no_of_gen; i++) {
-            mprices.add(deviP(calresult,i,t,1));
+            mprices.add(deviP(calresult, i, t, 1));
         }
         return Collections.min(mprices);
     }
 
     public double[] getMpriceTs(Calresult result) {
-        int no_of_ti=scucData.getTiNum();
-        double[] mprices=new double[no_of_ti];
+        int no_of_ti = scucData.getTiNum();
+        double[] mprices = new double[no_of_ti];
         for (int t = 0; t < no_of_ti; t++) {
-            mprices[t]=getMpriceT(result,t);
+            mprices[t] = getMpriceT(result, t);
         }
         return mprices;
     }
 
-    public double deviP(Calresult calresult,int i,int t,int order) {
+    public double deviP(Calresult calresult, int i, int t, int order) {
         Generator[] gens = scucData.getGens();
         GeneratorWithQuadraticCostCurve gen = (GeneratorWithQuadraticCostCurve) gens[i];
-        int[][] genStatus=calresult.getGenStatus();
-        int[][] genY=calresult.getGenY();
-        double[][] genOutput=calresult.getGenOutput();
-        if(miss_Wrong(gen,genStatus,genOutput,i,t)){
+        int[][] genStatus = calresult.getGenStatus();
+        int[][] genY = calresult.getGenY();
+        double[][] genOutput = calresult.getGenOutput();
+        if (miss_Wrong(gen, genStatus, genOutput, i, t)) {
             return penalty;
         }
-        DerivativeStructure pi_t=new DerivativeStructure(1,3,0,genOutput[i][t]);
+        DerivativeStructure pi_t = new DerivativeStructure(1, 3, 0, genOutput[i][t]);
 //        get f1
-        double constant1=getConstant(deepcopy2Dint(genStatus),genY,genOutput,i,t,1);
-        constant1=constant1+gen.getAConstant()+genY[i][t]*gen.getStartupCost();
-        DerivativeStructure f1_=new DerivativeStructure(gen.getAQuadratic(), pi_t.multiply(pi_t), gen.getALinear(), pi_t);
-        DerivativeStructure f1=f1_.add(constant1);
+        double constant1 = getConstant(deepcopy2Dint(genStatus), genY, genOutput, i, t, 1);
+        constant1 = constant1 + gen.getAConstant() + genY[i][t] * gen.getStartupCost();
+        DerivativeStructure f1_ = new DerivativeStructure(gen.getAQuadratic(), pi_t.multiply(pi_t), gen.getALinear(), pi_t);
+        DerivativeStructure f1 = f1_.add(constant1);
 
 //        get f2
-        double constant2=getConstant(deepcopy2Dint(genStatus),genY,genOutput,i,t,2);
-        constant2=constant2+gen.getGasc();
-        DerivativeStructure f2_=new DerivativeStructure(gen.getGasa(), pi_t.multiply(pi_t), gen.getGasb(), pi_t);
-        DerivativeStructure f2=f2_.add(constant2);
+        double constant2 = getConstant(deepcopy2Dint(genStatus), genY, genOutput, i, t, 2);
+        constant2 = constant2 + gen.getGasc();
+        DerivativeStructure f2_ = new DerivativeStructure(gen.getGasa(), pi_t.multiply(pi_t), gen.getGasb(), pi_t);
+        DerivativeStructure f2 = f2_.add(constant2);
 
 //        find f=f1^2+f2^2
-        if(calresult.getTargetflag()!=scucData.getTargetflag()){
+        if (calresult.getTargetflag() != scucData.getTargetflag()) {
             throw new java.lang.Error("Targetflag must be the same");
         }
-        DerivativeStructure f=null;
-        if(calresult.getTargetflag()!=3){
-            f=new DerivativeStructure(1, f1.multiply(f1), 1, f2.multiply(f2));
-            throw new java.lang.Error("haven't done yet");
-        }else {
-            double result1=scucData.getResult1().getBestObjValue();
-            double result2=scucData.getResult2().getBestObjValue();
-            f=new DerivativeStructure(1/(result1*result1), f1.multiply(f1), 1/(result2*result2), f2.multiply(f2));
+        DerivativeStructure f = null;
+        if (calresult.getTargetflag() == 1) {
+            f = f1;
+        } else if (calresult.getTargetflag() == 2) {
+            f = f2;
+        } else if (calresult.getTargetflag() == 3) {
+            double result1 = scucData.getResult1().getBestObjValue();
+            double result2 = scucData.getResult2().getBestObjValue();
+            f = new DerivativeStructure(1 / (result1 * result1), f1.multiply(f1), 1 / (result2 * result2), f2.multiply(f2));
+        } else {
+            throw new java.lang.Error("Targetflag is not right!");
         }
 
         return f.getPartialDerivative(order);
     }
 
-    private boolean miss_Wrong(GeneratorWithQuadraticCostCurve gen, int[][] genStatus, double[][] genOutput, int i, int t) {
-        if(genStatus[i][t]==0){
+    public boolean miss_Wrong(GeneratorWithQuadraticCostCurve gen, int[][] genStatus, double[][] genOutput, int i, int t) {
+        if (genStatus[i][t] == 0) {
             return true;
         }
-        if(gen.getMaxP()-genOutput[i][t]<3){
+        if (gen.getMaxP() - genOutput[i][t] < 3) {
             return true;
         }
         return false;
     }
 
-    private double getConstant(int[][] fakeStatus,int[][] genY, double[][] genOutput, int i, int t,int targetflag) {
-        fakeStatus[i][t]=0;
-        return Tools.getObjValue(fakeStatus,genY,genOutput,scucData,targetflag);
+    private double getConstant(int[][] fakeStatus, int[][] genY, double[][] genOutput, int i, int t, int targetflag) {
+        fakeStatus[i][t] = 0;
+        return Tools.getObjValue(fakeStatus, genY, genOutput, scucData, targetflag);
     }
 
 
--- src/sjtu/scuc/academic/test/DifferentiationTest.java
+++ src/sjtu/scuc/academic/test/DifferentiationTest.java
@@ -20,6 +20,7 @@ public class DifferentiationTest {
     MIPGurobi scucAlg;
     SCUCSolver scucSolver;
     Calresult result;
+
     @Before
     public void setUp() throws Exception {
 
@@ -31,32 +32,66 @@ public class DifferentiationTest {
 
     @Test
     public void deepcopy2Dint() throws Exception {
-        int[][] hehe=new int[3][4];
-        int[][] deeped=devi.deepcopy2Dint(hehe);
-        assertTrue((hehe[1][2]==deeped[1][2]));
-        deeped[1][2]=31;
-        assertFalse((hehe[1][2]==deeped[1][2]));
-        assertTrue((hehe[1][1]==deeped[1][1]));
+        int[][] hehe = new int[3][4];
+        int[][] deeped = devi.deepcopy2Dint(hehe);
+        assertTrue((hehe[1][2] == deeped[1][2]));
+        deeped[1][2] = 31;
+        assertFalse((hehe[1][2] == deeped[1][2]));
+        assertTrue((hehe[1][1] == deeped[1][1]));
     }
 
     @Test
     public void deviP1() throws Exception {
         prepare4data();
-        System.out.println(devi.deviP(result,3,2,1));
+        System.out.println(devi.deviP(result, 3, 2, 1));
+    }
+
+    @Test
+    public void deviP2() throws Exception {
+        prepare4data();
+        scucData.setTargetflag(1);
+        result.setTargetflag(1);
+
+        int t = 3;
+        int i = 0;
+
+        System.out.println(devi.deviP(result, i, t, 1));
+
+        Generator[] gens = scucData.getGens();
+        GeneratorWithQuadraticCostCurve gen = (GeneratorWithQuadraticCostCurve) gens[i];
+        while (devi.miss_Wrong(gen, result.getGenStatus(), result.getGenOutput(), i, t) == true) {
+            i += 1;
+//            寻找边际机组
+            gen = (GeneratorWithQuadraticCostCurve) gens[i];
+        }
+
+        double p = result.getGenOutput()[i][t];
+        assertEquals(devi.deviP(result, i, t, 1), (2 * gen.getAQuadratic() * p + gen.getALinear()));
     }
 
     @Test
     public void getMpriceT() throws Exception {
         prepare4data();
-        System.out.println(devi.getMpriceT(result,3));
+        System.out.println(devi.getMpriceT(result, 3));
     }
 
     @Test
     public void getMpriceTs() throws Exception {
         prepare4data();
-        double[] mprices=devi.getMpriceTs(result);
-        for (int i = 0; i <mprices.length; i++) {
-            System.out.print(Double.toString(mprices[i])+" ");
+        double[] mprices = devi.getMpriceTs(result);
+        for (int i = 0; i < mprices.length; i++) {
+            System.out.print(Double.toString(mprices[i]) + " ");
+        }
+    }
+
+    @Test
+    public void getMpriceTs2() throws Exception {
+        prepare4data();
+        scucData.setTargetflag(1);
+        result.setTargetflag(1);
+        double[] mprices = devi.getMpriceTs(result);
+        for (int i = 0; i < mprices.length; i++) {
+            System.out.print(Double.toString(mprices[i]) + " ");
         }
     }
 
@@ -70,18 +105,18 @@ public class DifferentiationTest {
         scucData.setReserve(reserve);
         scucData.setMode("f1");
         scucSolver = new SCUCSolver();
-        scucAlg=new MIPGurobi();
+        scucAlg = new MIPGurobi();
 
         scucSolver.setScucAlg(scucAlg);
         scucData.setTargetflag(1);
-        Calresult result1=scucSolver.optimize(scucData);
+        Calresult result1 = scucSolver.optimize(scucData);
         scucData.setTargetflag(2);
-        Calresult result2=scucSolver.optimize(scucData);
+        Calresult result2 = scucSolver.optimize(scucData);
         scucData.setResult1(result1);
         scucData.setResult2(result2);
         scucData.setTargetflag(3);
-        result=scucSolver.optimize(scucData);
+        result = scucSolver.optimize(scucData);
 
-        devi=new Differentiation(scucData);
+        devi = new Differentiation(scucData);
     }
 }
